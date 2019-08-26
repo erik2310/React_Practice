0fffb8ce1258c8168950162d4c85430960b07392
Erik Ruhmanis <erik2310@edu.easj.dk>
2019-08-26 11:42:12 +0200

    Slettet ekstra node_modules mappe.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+function Clock(props) {
     return (
         <div>
-            <h1>Hello World!</h1>
-            <p>This is my first React app.</p>
+            <p>The time is {props.date.toLocaleTimeString()}.</p>
         </div>
     );
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+function App() {
+    return (
+        <div className="mt-3 ml-3">
+            <h1>Hello World!</h1>
+            <p className="mt-3">This is my first React app.</p>
+            <Clock date={new Date()} />
+        </div>
+    );
+}
+
+function tick() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
